c9a13ec6a9c045670bece6dd73601b88aeb3a663
Arcxwur <arcxwur@gmail.com>
2023-04-18 13:36:34 -0300

    DP

---
+++ Project1/Sing.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Configuracoes configuracoes = Configuracoes.GetInstancia();
+
+        configuracoes.SetConfiguracao("idioma", "portugues");
+
+        Console.WriteLine("Idioma: " + configuracoes.GetConfiguracao("idioma"));
+
+        Configuracoes outraConfiguracoes = Configuracoes.GetInstancia();
+        if (configuracoes == outraConfiguracoes)
+        {
+            Console.WriteLine("As duas variáveis apontam para a mesma instância da classe Configuracoes.");
+        }
+        else
+        {
+            Console.WriteLine("As duas variáveis apontam para instâncias diferentes da classe Configuracoes.");
+        }
+    }
+}
+
+public class Configuracoes
+{
+    private static Configuracoes instancia = null;
+    private readonly string[] configuracoes = new string[10];
+
+    private Configuracoes()
+    {
+
+    }
+
+    public static Configuracoes GetInstancia()
+    {
+        if (instancia == null)
+        {
+            instancia = new Configuracoes();
+        }
+        return instancia;
+    }
+    public void SetConfiguracao(string nome, string valor)
+    {
+        configuracoes[nome.GetHashCode() % 10] = valor;
+    }
+    public string GetConfiguracao(string nome)
+    {
+        return configuracoes[nome.GetHashCode() % 10];
+    }
+}
+
+
---
+++ Project2/Decorator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Texto texto = new Texto("Exemplo de texto");
+            NegritoTexto negrito = new NegritoTexto(texto);
+            ItalicoTexto italico = new ItalicoTexto(negrito);
+            string textoDecorado = italico.GetTexto();
+            Console.WriteLine(textoDecorado);
+        }
+    }
+
+    public interface ITexto
+    {
+        string GetTexto();
+    }
+
+    public class Texto : ITexto
+    {
+        private string texto;
+
+        public Texto(string texto)
+        {
+            this.texto = texto;
+        }
+
+        public string GetTexto()
+        {
+            return texto;
+        }
+    }
+
+    public abstract class DecoratorTexto : ITexto
+    {
+        protected ITexto texto;
+
+        public DecoratorTexto(ITexto texto)
+        {
+            this.texto = texto;
+        }
+
+        public virtual string GetTexto()
+        {
+            return texto.GetTexto();
+        }
+    }
+
+    public class NegritoTexto : DecoratorTexto
+    {
+        public NegritoTexto(ITexto texto) : base(texto)
+        {
+        }
+
+        public override string GetTexto()
+        {
+            return "<b>" + base.GetTexto() + "</b>";
+        }
+    }
+
+    public class ItalicoTexto : DecoratorTexto
+    {
+        public ItalicoTexto(ITexto texto) : base(texto)
+        {
+        }
+
+        public override string GetTexto()
+        {
+            return "<i>" + base.GetTexto() + "</i>";
+        }
+    }
+
+}
